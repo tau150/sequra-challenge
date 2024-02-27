961979015887bbaad9e055b0c7dc678351602c52
Santiago Nuñez <tau150@hotmail.com>
2024-02-27 12:58:29 +0100

    fetcher implementation and global test config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,21 @@
+/// <reference types="vitest" />
+
 import path from "path";
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.js",
+    include: ["**/*.test.?(c|m)[jt]s?(x)"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
